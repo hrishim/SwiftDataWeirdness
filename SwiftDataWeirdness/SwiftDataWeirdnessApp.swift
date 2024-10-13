274af9481a3c720d0b1266b8ccef74231cf30c9b
hrishim <hrishim@gmail.com>
2024-10-13 19:32:14 +0530

    Initial Commit

---
+++ SwiftDataWeirdness/SwiftDataWeirdnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataWeirdnessApp.swift
+//  SwiftDataWeirdness
+//
+//  Created by Hrishikesh on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataWeirdnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
